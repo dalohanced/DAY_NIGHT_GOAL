cdb4fc9aa2e95b419df504ce02d24809a077b9e8
sak1133 <72155396+sak1133@users.noreply.github.com>
2022-04-16 23:51:01 +0300

    Bot
    Пока что временное ограничение около 100 секунд.

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import time
+from telebot import types
+bot = telebot.TeleBot(open("key12.txt").readline())
+
+
+murkup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+item1=types.KeyboardButton("/1")
+item2=types.KeyboardButton("")
+item3=types.KeyboardButton("")
+item4=types.KeyboardButton("")
+
+murkup.add(item1,item2,item3,item4)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+
+    bot.send_message(message.chat.id,'Привет, этот бот будет напоминать тебе о твоей цели,напиши ее мне',reply_markup=murkup)
+
+@bot.message_handler(content_types=['text'])
+
+
+@bot.message_handler(commands=['1'])
+def bred(message):
+
+    bot.send_message(message.chat.id,"Твоя цель на сегодня:",reply_markup=murkup)
+    time.sleep(100)
+    txt = message.text
+    a=txt
+
+    bot.send_message(message.chat.id,a)
+
+bot.polling(none_stop=True, interval=0)
